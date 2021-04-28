c1708d3c4d45d4d25f8d76cbfc01c529b43771dd
cpyberry <cpyberry222@gmail.com>
2021-04-28 23:04:33 +0900

    feat: add a function get_result_path_pair
    Whether or not SHGetSpecialFolderPathA was successful is stored in the first of std::pair, and the acquired path is stored in the second.

--- include/pathwrap/pathwrap.hpp
+++ include/pathwrap/pathwrap.hpp
@@ -2,4 +2,23 @@
 #define _PATHWRAP_HPP
 
 
+#include <filesystem>
+#include <utility>
+
+#include <ShlObj.h>
+
+
+namespace pathwrap {
+using PathType = std::pair<const bool, const std::filesystem::path>;
+
+
+inline PathType get_result_path_pair(const int& csidl) noexcept
+{
+	char path[MAX_PATH] = {};
+	bool result = SHGetSpecialFolderPathA(nullptr, path, csidl, false);
+	return std::make_pair(result, std::filesystem::path(path));
+}
+}  // namespace pathwrap
+
+
 #endif  // _PATHWRAP_HPP
