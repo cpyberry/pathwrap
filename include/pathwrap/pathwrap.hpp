b9aa262cc2c0129e9451f3d702f65301987e6b7e
cpyberry <cpyberry222@gmail.com>
2021-04-28 23:13:09 +0900

    feat: add a function get_special_path

--- include/pathwrap/pathwrap.hpp
+++ include/pathwrap/pathwrap.hpp
@@ -3,6 +3,7 @@
 
 
 #include <filesystem>
+#include <stdexcept>
 #include <utility>
 
 #include <ShlObj.h>
@@ -17,6 +18,17 @@ inline PathType get_result_path_pair(const int& csidl) noexcept
 	char path[MAX_PATH] = {};
 	bool result = SHGetSpecialFolderPathA(nullptr, path, csidl, false);
 	return std::make_pair(result, std::filesystem::path(path));
+}
+
+
+inline std::filesystem::path get_special_path(const int& csidl)
+{
+	const auto [result, path] = get_result_path_pair(csidl);
+	if (result) {
+		return std::filesystem::path(path);
+	} else {
+		throw std::runtime_error("Failed to get directory path");
+	}
 }
 }  // namespace pathwrap
 
