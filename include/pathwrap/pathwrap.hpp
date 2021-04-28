76400eca6c8f225fe47a088fbbf914bd8e4a01f2
cpyberry <cpyberry222@gmail.com>
2021-04-29 00:46:59 +0900

    docs: add NOTICE at the beginning of the file

--- include/pathwrap/pathwrap.hpp
+++ include/pathwrap/pathwrap.hpp
@@ -1,3 +1,13 @@
+/*
+Copyright 2021 cpyberry
+https://github.com/cpyberry/pathwrap
+
+cpyberry
+email: cpyberry222@gmail.com
+github: https://github.com/cpyberry
+*/
+
+
 #ifndef _PATHWRAP_HPP
 #define _PATHWRAP_HPP
 
